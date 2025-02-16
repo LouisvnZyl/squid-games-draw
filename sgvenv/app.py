b159e101f5e57ac703e6d1caa14a668c8b397cb9
Louis van Zyl <louisvanzyl@outlook.com>
2025-02-16 15:52:08 +0200

    Add and implement the video streaming endpoint python

--- sgvenv/app.py
+++ sgvenv/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from Endpoints.health import health_routes
+from Endpoints.video_streaming import video_routes
 
 app = Flask(__name__)
 
 app.register_blueprint(health_routes, url_prefix="/health")
+app.register_blueprint(video_routes, url_prefix='/video')
 
 if __name__ == "__main__":
     app.run(debug=True)
