212d1038b06244057e6413380b18123d1aae8f59
Louis van Zyl <louisvanzyl@outlook.com>
2025-03-02 13:24:44 +0200

    Code cleanup and file renaming

--- sgvenv/Endpoints/image_evaluation.py
+++ sgvenv/Endpoints/image_evaluation.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, Response
-from Services.ImageService import get_latest_drawn_image
+from Services.ImageService import get_latest_drawn_image_bytes
 
 image_evaluation_routes = Blueprint('image_evaluation_routes', __name__)
 
 @image_evaluation_routes.route('/drawn-image', methods=['GET'])
 def drawn_image():
-    image_blob = get_latest_drawn_image()
+    image_blob = get_latest_drawn_image_bytes()
 
     return Response(image_blob, mimetype='image/png')
--- sgvenv/Services/ImageService.py
+++ sgvenv/Services/ImageService.py
@@ -1,32 +1,8 @@
 import cv2
-import numpy as np
 import io
 from PIL import Image
 
-def initialize_video_stream(sourceIndex: int) -> cv2.VideoCapture:
-    cap = cv2.VideoCapture(sourceIndex)
-    
-    return cap
-    
-def create_painting_canvas() -> np.ndarray:
-    paintWindow = np.zeros((471, 636, 3)) + 255
-    
-    return paintWindow
-    
-def build_stream_frames(cap: cv2.VideoCapture):    
-    # Read each frame from the capture source
-    ret, frame = cap.read()
-    
-    # Flip the frame vertically
-    frame = cv2.flip(frame, 1)
-    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    return ret, frame, framergb
-
-def display_frame(frameName: str ,frame):
-    cv2.imshow(frameName, frame)
-
-def get_latest_drawn_image():
+def get_latest_drawn_image_bytes():
     image = cv2.imread("DrawnImages/drawing_output.png")
 
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
