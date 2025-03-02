67e1c105d5cfa9c51267e216a40287b693bc0538
Louis van Zyl <louisvanzyl@outlook.com>
2025-03-02 14:31:18 +0200

    Add endpoint to call for the image evaluation

--- sgvenv/Endpoints/image_evaluation.py
+++ sgvenv/Endpoints/image_evaluation.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, Response
 from Services.ImageService import get_latest_drawn_image_bytes
+from Services.ImageEvaluationService import predict_image
 
 image_evaluation_routes = Blueprint('image_evaluation_routes', __name__)
 
@@ -8,3 +9,9 @@ def drawn_image():
     image_blob = get_latest_drawn_image_bytes()
 
     return Response(image_blob, mimetype='image/png')
+
+@image_evaluation_routes.route('/evaluate-image', methods=['GET'])
+def evaluate_image():
+    evaluation_result = predict_image("DrawnImages/drawing_output.png")
+
+    return Response(evaluation_result, content_type='text/plain')
